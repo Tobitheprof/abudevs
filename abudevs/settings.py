a992867a6bdcbb7d24df07a2bd85180d26ca3159
Tobi TheRevolutionary <tobiekundayo07@gmail.com>
2023-02-25 22:58:07 +0100

    Prep for Production

--- abudevs/settings.py
+++ abudevs/settings.py
@@ -156,4 +156,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tobiekundayo07@gmail.com'
-EMAIL_HOST_PASSWORD = 'ydabeddxobvoiazd'
+EMAIL_HOST_PASSWORD = 'ukjufbrzadymueti'
